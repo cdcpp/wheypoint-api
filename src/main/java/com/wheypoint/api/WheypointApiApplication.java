71472b7582a147b19f6bfc38a20f8883f6774335
cdcpp <oblilic93@gmail.com>
2026-01-11 16:22:30 +0900

    first

---
+++ src/main/java/com/wheypoint/api/WheypointApiApplication.java
@@ -0,0 +1,13 @@
+package com.wheypoint.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheypointApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheypointApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wheypoint/api/WheypointApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheypoint.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheypointApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
